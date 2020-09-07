27fb0fcf97574eccef60e4f25de445c63294cb7c
Pål Grønås Drange <pgdr@equinor.com>
2020-09-07 15:39:37 +0200

    Create new file

---
+++ script.py
@@ -0,0 +1,19 @@
+# Weekday
+
+cauliflower soup
+vegetarian hamburger
+vegan hamburger
+pommes frites
+pizza spicy margerita
+
+# Weekends
+
+Vegetarian pizza
+Beyond Hamburgers
+Taco vegan style
+
+# Sunday special
+
+Meatballs
+Fries
+Ice cream
